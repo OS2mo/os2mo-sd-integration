909ed7a99711b60cd982c4be7f6639c0a3fb6cd7
Andreas Kring <andreas@magenta.dk>
2024-09-25 13:40:12 +0200

    refactor: move build_timeline into class

--- sdlon/sd/timeline.py
+++ sdlon/sd/timeline.py
@@ -1,37 +1,40 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-from datetime import date, datetime
-from zoneinfo import ZoneInfo
+from datetime import date
+from datetime import datetime
 
 from sdclient.client import SDClient
 from sdclient.responses import EmploymentWithLists
-
+from zoneinfo import ZoneInfo
 
 TIMEZONE = ZoneInfo("Europe/Copenhagen")
 
 
-def build_timeline(
-    sd_client: SDClient,
-    inst_id: str,
-    start_date: date,
-    cpr: str,
-    emp_id: str,
-) -> EmploymentWithLists:
-    """
-    Build the entire employment timeline for a given person and employment.
+class SD:
+    def __init__(self, username: str, password: str, institution_identifier: str):
+        self.institution_identifier = institution_identifier
+        self.client = SDClient(username, password)
 
-    Args:
-        sd_client: The SDClient.
-        inst_id: The SD InstitutionIdentifier.
-        start_date: The date back to which we should at least include SD employment
-            data. The start_date must be less than or equal to today.
-        cpr: CPR-number of the person.
-        emp_id: The SD EmploymentIdentifier for the persons employment.
+    def build_timeline(
+        self,
+        start_date: date,
+        cpr: str,
+        emp_id: str,
+    ) -> EmploymentWithLists:
+        """
+        Build the entire employment timeline for a given person and employment.
 
-    Returns:
-        Entire employment timeline.
-    """
+        Args:
+            sd_client: The SDClient.
+            inst_id: The SD InstitutionIdentifier.
+            start_date: The date back to which we should at least include SD employment
+                data. The start_date must be less than or equal to today.
+            cpr: CPR-number of the person.
+            emp_id: The SD EmploymentIdentifier for the persons employment.
 
-    now = datetime.now(tz=TIMEZONE).date()
-    assert start_date <= now, "start_date must be less than or equal to today"
+        Returns:
+            Entire employment timeline.
+        """
 
+        now = datetime.now(tz=TIMEZONE).date()
+        assert start_date <= now, "start_date must be less than or equal to today"
--- tests/sd/test_timeline.py
+++ tests/sd/test_timeline.py
@@ -1,18 +1,18 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 from datetime import date
-from unittest.mock import MagicMock
 
 import pytest
 from freezegun import freeze_time
 
-from sdlon.sd.timeline import build_timeline
+from sdlon.sd.timeline import SD
 
 
 @freeze_time("2024-01-01")
 def test_build_timeline_start_date_ok() -> None:
+    # Arrange
+    sd = SD("username", "password", "II")
+
     # Act + Assert
-    with pytest.raises(AssertionError) as error:
-        build_timeline(
-            MagicMock(), "II", date(2025, 1, 1), "0101011234", "12345"
-        )
+    with pytest.raises(AssertionError):
+        sd.build_timeline(date(2025, 1, 1), "0101011234", "12345")
